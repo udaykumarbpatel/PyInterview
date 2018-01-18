ca2ac61bac34eb39a210a66268733aa6f7a17c52
Uday <udaykumarbpatel@gmail.com>
2018-01-17 19:42:31 -0800

    Added More

---
+++ InterviewCake/InfoGraphicNoBuildIn.py
@@ -0,0 +1,40 @@
+def input_read(input):
+    clean_input = ""
+    result = {}
+    word = ""
+
+    for c in input:
+        if not c.isalpha() and not c.isspace():
+            c = " "
+        clean_input += c
+
+    input = clean_input
+    clean_input = ""
+
+    for c in input:
+        if c.isupper():
+            c = c.swapcase()
+        clean_input += c
+
+    for c in clean_input:
+        if c.isspace():
+            if len(word) > 0:
+                if word in result:
+                    result.update({word: result.get(word) + 1})
+                else:
+                    result.update({word: 1})
+                word = ""
+        else:
+            word += c
+
+    if len(word) > 0:
+        if word in result:
+            result.update({word: result.get(word) + 1})
+        else:
+            result.update({word: 1})
+
+    return result
+
+
+print input_read('We came, we saw, we conqueredthen we ate Bill\'s (Mille-Feuille) cake.'
+                 'The bill came to five dollars.')
---
+++ InterviewCake/TrieImplementation.py
@@ -0,0 +1,28 @@
+_end = '*'
+
+
+def make_trie(*words):
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
+    print root
+    return root
+
+
+def in_trie(trie, word):
+    current_dict = trie
+    for letter in word:
+        if letter in current_dict:
+            current_dict = current_dict[letter]
+        else:
+            return False
+    else:
+        if _end in current_dict:
+            return True
+        else:
+            return False
+
+print in_trie(make_trie('foo', 'bar', 'baz', 'barz'), 'baz')
